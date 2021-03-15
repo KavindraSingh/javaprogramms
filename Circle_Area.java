4a06c4cb788a18de6bb1b1660daabed5abbd87ea
KavindraSingh <76774310+KavindraSingh@users.noreply.github.com>
2021-03-15 07:48:04 +0530

    Add files via upload

---
+++ Circle_Area.java
@@ -0,0 +1,18 @@
+package project2;
+
+
+public class Class1 {
+	float pi =3.14f;
+	float r=7f;
+	float a=pi*r*r;
+	void  display() {
+	 
+	}
+
+public static void main(String[] args) {
+	Class1 n=new Class1();
+	System.out.print("Area of circle is  ");
+	System.out.print(n.a);
+	n.display();
+	}
+}
